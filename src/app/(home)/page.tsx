dbc44e373bd00f88eed80bd654cf3b93e4e0f29a
Jonntz <monteirojonatas23@gmail.com>
2023-10-26 01:53:49 -0300

    adding second banner on homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-6">
+        <p className='font-bold uppercase pl-5 mb-1'>Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image 
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        alt='até 55% de desconto em mouses'
+        className='h-auto w-full px-5'
+        sizes='100vw'
+      />
     </div>
   )
 }
