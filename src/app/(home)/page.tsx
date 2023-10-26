6e9100cdc268adfb94ad272d412c875ec9f984d8
Jonntz <monteirojonatas23@gmail.com>
2023-10-26 01:37:02 -0300

    fixing import on page component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,34 @@
-"use client"
-import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Categories from './components/categories'
+import { db } from '@/lib/prisma'
+import ProductList from './components/product-list'
 
-export default function Home() {
-  
+export default async function Home() {
+  const deals = await db.product.findMany({
+    where:{
+      discountPercentage:{
+        gt: 0
+      }
+    }
+  })
+
   return (
-    <div className='p-5'>
+    <div>
       <Image 
         src="/banner-home-01.png"
         height={0}
         width={0}
         alt='até 55% de desconto esse mês'
-        className='h-auto w-full'
+        className='h-auto w-full px-5'
         sizes='100vw'
       />
 
-      <div className="mt-6">
+      <div className="mt-6 px-5">
         <Categories />
+      </div>
+
+      <div className="mt-6">
+        <ProductList products={deals} />
       </div>
     </div>
   )
