1a64100016817d0e9ff80a9010d965d696502e95
Jonntz <monteirojonatas23@gmail.com>
2023-10-28 01:41:54 -0300

    Fixing product item size

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -10,8 +10,8 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
-        <div className="flex flex-col max-w-[156px] gap-4">
-            <div className="relative bg-accent rounded-lg h-[170px] w-[156px] flex justify-center items-center">
+        <div className="flex flex-col max-w-[170px] gap-4">
+            <div className="relative bg-accent rounded-lg h-[170px] w-[170px] flex justify-center items-center">
                 <Image 
                     src={product.imageUrls[0]} 
                     height={0} width={0} 
@@ -32,23 +32,23 @@ const ProductItem = ({ product }: ProductItemProps) => {
                     
 
             <div>
-                <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis w-full"> 
+                <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis"> 
                     {product.name} 
                 </p>
                 
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 ">
                     {product.discountPercentage > 0 ? (
                         <>
-                            <p className="font-semibold"> 
+                            <p className="font-semibold overflow-hidden whitespace-nowrap text-ellipsis"> 
                                 R$ {Number(product.totalPrice).toFixed(2) } 
                             </p>
                             
-                            <p className="opacity-75 line-through text-xs"> 
+                            <p className="opacity-75 line-through text-xs overflow-hidden whitespace-nowrap text-ellipsis"> 
                                 R$ {Number(product.basePrice).toFixed(2) } 
                             </p>
                         </>
                     ) : (
-                        <p className="opacity-75 line-through text-xs"> 
+                        <p className="opacity-75 line-through text-xs overflow-hidden whitespace-nowrap text-ellipsis"> 
                             R$ {Number(product.basePrice).toFixed(2) } 
                         </p>
                     )}
