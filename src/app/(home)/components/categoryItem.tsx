98477230b54031cdb48550a1ea2df7542a47ddf6
Jonntz <monteirojonatas23@gmail.com>
2023-11-03 20:33:33 -0300

    Add homepage category item link

--- src/app/(home)/components/categoryItem.tsx
+++ src/app/(home)/components/categoryItem.tsx
@@ -1,6 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-items";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CategoryItemsProps {
     category: Category
@@ -9,10 +10,12 @@ interface CategoryItemsProps {
 const CategoryItem = ({category}: CategoryItemsProps) => {
     
     return ( 
-        <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
-            {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-            <span className="font-bold text-xs" >{category.name}</span>
-        </Badge>
+        <Link href={`/category/${category.slug}`}>
+            <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
+                {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+                <span className="font-bold text-xs" >{category.name}</span>
+            </Badge>
+        </Link>
      );
 }
  
