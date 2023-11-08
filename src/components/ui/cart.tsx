421ad02d0e7cdb9ea535f3c9dc51212c736d6756
Jonntz <monteirojonatas23@gmail.com>
2023-11-08 16:50:29 -0300

    Add scroll area in cart component

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,25 +5,31 @@ import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { computeTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
     const {products, total, subtotal, discountTotal} = useContext(CartContext)
 
     return ( 
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 h-full">
             <Badge className="gap-1 w-fit text-base uppercase border-primary border-2 px-3 py-[0.375rem]" variant="outline">
                 <ShoppingCartIcon size={16}/> Carrinho
             </Badge>
 
-            <div className="flex flex-col gap-5">
-                {products.length > 0 ? (
-                    products.map(product => (
-                        <CartItem key={product.id} product={computeTotalPrice(product as any) as any} />
-                        
-                    ))
-                ) : (
-                    <p className="text-center font-semibold">O Carrinho está vazio</p>
-                )}
+            <div className="flex flex-col gap-5 h-full max-h-full overflow-hidden">
+                <ScrollArea className="h-full">
+                    <div className="flex h-full flex-col gap-8">
+                        {products.length > 0 ? (
+                            products.map(product => (
+                                <CartItem key={product.id} product={computeTotalPrice(product as any) as any} />
+                                
+                            ))
+                        ) : (
+                            <p className="text-center font-semibold">O Carrinho está vazio</p>
+                        )}
+                    </div>
+                </ScrollArea>
             </div>
 
             <div className="flex flex-col gap-3">
@@ -54,6 +60,10 @@ const Cart = () => {
                     <p>Total</p>
                     <p>R$ {total.toFixed(2)}</p>
                 </div>
+
+                <Button className="font-bold uppercase mt-7">
+                    Finalizar compra
+                </Button>
             </div>
         </div>
      );
