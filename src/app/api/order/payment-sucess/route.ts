29182e1a7ef7262ca28b3e3ee1f3b4587ac95748
Jonntz <monteirojonatas23@gmail.com>
2023-11-09 17:45:43 -0300

    testing

--- src/app/api/order/payment-sucess/route.ts
+++ src/app/api/order/payment-sucess/route.ts
@@ -1,40 +1,40 @@
-import { NextResponse } from "next/server"
-import Stripe from "stripe"
+// import { NextResponse } from "next/server"
+// import Stripe from "stripe"
 
-const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
-    apiVersion: '2023-10-16',
-})
+// const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
+//     apiVersion: '2023-10-16',
+// })
 
-export const POST = async (request: Request) => {
-    const signature = request.headers.get('stripe-signature')
+// export const POST = async (request: Request) => {
+//     const signature = request.headers.get('stripe-signature')
 
-    if (!signature) {
-        return NextResponse.error()
-    }
+//     if (!signature) {
+//         return NextResponse.error()
+//     }
 
-    const text = await request.text()
+//     const text = await request.text()
 
-    const event = stripe.webhooks.constructEvent(
-        text,
-        signature,
-        process.env.STRIPE_WEBHOOK_SECRET_KEY
-    )
+//     const event = stripe.webhooks.constructEvent(
+//         text,
+//         signature,
+//         process.env.STRIPE_WEBHOOK_SECRET_KEY
+//     )
 
-    if (event.type === 'checkout.session.completed') {
+//     if (event.type === 'checkout.session.completed') {
 
-        const sessionWithLineitems = await stripe.checkout.sessions.retrieve(
-            event.data.object.id,
-            {
-                expand: ['line-items']
-            }
-        )
+//         const sessionWithLineitems = await stripe.checkout.sessions.retrieve(
+//             event.data.object.id,
+//             {
+//                 expand: ['line-items']
+//             }
+//         )
 
-        const lineItems = sessionWithLineitems.line_items
+//         const lineItems = sessionWithLineitems.line_items
 
-        //Criar uma Order no prisma 
-        // Registrar os pedidos no prisma 
-        // Criar a tela de pedidos
-    }
+//         //Criar uma Order no prisma
+//         // Registrar os pedidos no prisma
+//         // Criar a tela de pedidos
+//     }
 
-    return NextResponse.json({ received: true })
-} 
+//     return NextResponse.json({ received: true })
+// } 
