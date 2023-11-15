88a8b3a052913c6829a594e90835b8ea2d6c2aac
Jonntz <monteirojonatas23@gmail.com>
2023-11-14 21:07:10 -0300

    ensure a categories are responsives on desktop

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -4,7 +4,7 @@ import CategoryItem from "./categoryItem";
 const Categories = async () => {
     const categories = await db.category.findMany({})
     return ( 
-        <div className="grid grid-cols-2 gap-y-2 gap-x-4">
+        <div className="grid grid-cols-2 gap-y-2 gap-x-4 lg:grid-cols-6">
             {categories.map((category) => (
                 <CategoryItem key={category.id} category={category} />
             ))}
